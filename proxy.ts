5a18986fa681044900aeae06ccf72edfea05edba
Ritika <ritikasaini.ispier@gmail.com>
2026-09-19 11:30:44 +0530

    Fix Ritika subdomain routing

--- proxy.ts
+++ proxy.ts
@@ -1,16 +1,15 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function proxy(request: NextRequest) {
-  const hostname = request.headers.get("host")?.split(":")[0];
+  const hostname = request.nextUrl.hostname;
 
-  if (
-    hostname === "ritika.aavdigitalmarketing.co" &&
-    request.nextUrl.pathname === "/"
-  ) {
+  if (hostname === "ritika.aavdigitalmarketing.co") {
     const url = request.nextUrl.clone();
-    url.pathname = "/ritika";
 
-    return NextResponse.rewrite(url);
+    if (url.pathname === "/") {
+      url.pathname = "/ritika";
+      return NextResponse.rewrite(url);
+    }
   }
 
   return NextResponse.next();
